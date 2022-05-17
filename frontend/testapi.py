81ce4d2949073f6cd72ffc90c7897584e238255a
ahmed <ahm.798@pm.me>
2022-05-17 09:17:40 +0200

    adding article and topic models

--- frontend/testapi.py
+++ frontend/testapi.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/home/"
 
-response = requests.get(endpoint, json={"query": "hello world"})
+response = requests.get(endpoint, params={'q':"helloWorld"} ,json={"query": "hello world"})
 
 
 print(response.json())
